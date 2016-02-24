0b937e660d614cc7fd18391904c8c98bd35f0857
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-24 10:30:03 -0600

    comment guides on erb and controller

---
+++ web_app/controller.rb
@@ -0,0 +1,10 @@
+#Require Sinatra
+
+#Require any other documents the controller may depend on (**cough** models **/cough**)
+
+
+#Create an index Route
+
+
+#Create any other routes that make sense with the design of your application
+	#HINT: There should be at least two others
